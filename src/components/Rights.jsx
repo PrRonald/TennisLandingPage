001b38366c8463a705839c515a0dfa1ad6aa9422
PrRonald <ronaldne69@gmail.com>
2024-05-20 16:14:03 -0400

    the rigths component desktop view was made

--- src/components/Rights.jsx
+++ src/components/Rights.jsx
@@ -1,7 +1,8 @@
 export const Rights = () => {
     return (
         <section className="w-full h-auto bg-black" >
-            <ul className="flex flex-col justify-start gap-2 ml-6 py-2">
+            <ul className="flex flex-col xl:flex-row justify-start gap-2 ml-6 py-2
+                xl:gap-14">
                 <li className="text-white text-xs font-normal ">
                     2017 © PROSHOT. ALL RIGHTS RESERVED
                 </li>
