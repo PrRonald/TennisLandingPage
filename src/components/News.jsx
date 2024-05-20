8f7111f1671af7856f8f68bfec6a74ebaa523938
PrRonald <ronaldne69@gmail.com>
2024-05-20 15:44:41 -0400

    the news component desktop view was made

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -36,48 +36,54 @@ export const News = () => {
     ]
 
     return (
-        <section className="size-auto grid grid-rows-[auto_auto_auto] gap-14
-         bg-gray-300 gap px-6 py-3" >
-            <h1 className="w-auto text-xl font-semibold text-black text-left ">
-                LATEST NEWS
-            </h1>
-            <div className="flex flex-col gap-14">
-                {news.map(news => (
-                    <div className="w-full flex flex-col gap-4 " key={news.id}>
-                        <span className="w-14  h-1 bg-green-500" ></span>
-                        <div className="w-full flex flex-col justify-start gap-2 ">
-                            <h2 className="w-full text-sm font-medium text-black text-left ">
-                                {news.title}
-                            </h2>
-                            <h3 className="w-full text-xs font-normal text-gray-400 text-left ">
-                                {news.date}
-                            </h3>
+        <section className="w-full h-auto xl:h-[90vh] grid grid-rows-[auto_auto_auto] gap-14
+         bg-gray-300 xl:gap-0 px-6 py-3 xl:grid-rows-[20%_80%] xl:items-center " >
+                <h1 className="w-full text-xl xl:text-5xl font-semibold 
+                text-black text-left  ">
+                    LATEST NEWS
+                </h1>
+            <div className="w-full grid grid-rows-2 xl:grid-cols-[65%_35%] xl:grid-rows-1
+                 xl:place-items-center gap-14 xl:gap-0">
+                <div className="flex flex-col gap-14 xl:flex-row  ">
+                    {news.map(news => (
+                        <div className="w-full flex flex-col gap-4  "
+                            key={news.id}>
+                            <span className="w-14  h-1 bg-green-500" ></span>
+                            <div className="w-full flex flex-col justify-start gap-2 ">
+                                <h2 className="w-full text-sm font-medium text-black text-left ">
+                                    {news.title}
+                                </h2>
+                                <h3 className="w-full text-xs font-normal text-gray-400 text-left ">
+                                    {news.date}
+                                </h3>
+                            </div>
+                            <p className="w-full text-sm font-normal text-black text-justify  ">
+                                {news.news}
+                            </p>
                         </div>
-                        <p className="w-full text-sm font-normal text-black text-justify  ">
-                            {news.news}
-                        </p>
-                    </div>
-                ))}
-            </div>
-            <div className="h-auto flex flex-col items-start bg-white px-6 pt-7 pb-4 gap-10 ">
-                {games.map(game => (
-                    <div className="w-full h-[89px] flex flex-col place-items-start
+                    ))}
+                </div>
+                <div className="h-auto flex flex-col items-start bg-white
+                             px-6 pt-7 pb-4 gap-10 xl:w-[406px] xl:h-[370px] ">
+                    {games.map(game => (
+                        <div className="w-full h-[89px] flex flex-col place-items-start
                     border-b border-gray-300 gap-3 "
-                    key={game.id}>
-                        <h1 className="text-base font-normal text-black ">
-                            {game.title}
-                        </h1>
-                        <div className="w-full  flex flex-row gap-2 " >
-                            <SlCalender className="text-xs font-normal text-gray-400" />
-                            <h2 className="text-xs font-normal text-gray-400">
-                                {game.date}
-                            </h2>
+                            key={game.id}>
+                            <h1 className="text-base font-normal text-black ">
+                                {game.title}
+                            </h1>
+                            <div className="w-full  flex flex-row gap-2 " >
+                                <SlCalender className="text-xs font-normal text-gray-400" />
+                                <h2 className="text-xs font-normal text-gray-400">
+                                    {game.date}
+                                </h2>
+                            </div>
                         </div>
-                    </div>
-                ))}
-                <Link className="text-xs font-semibold text-green-700 " >
-                    VIEW ALL EVENTS
-                </Link>
+                    ))}
+                    <Link className="text-xs font-semibold text-green-700 " >
+                        VIEW ALL EVENTS
+                    </Link>
+                </div>
             </div>
         </section>
     );
