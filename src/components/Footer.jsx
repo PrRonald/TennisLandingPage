3a1e76a4d5bd1bd632312080e2aca8930844a485
PrRonald <ronaldne69@gmail.com>
2024-05-20 16:10:28 -0400

    the footer desktop view was made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ import { FaLocationDot } from "react-icons/fa6";
 
 export const Footer = () => {
   return (
-    <section className="mx-6">
+    <section className="mx-6 xl:my-6">
       <div className="w-full h-auto grid grid-cols-1 grid-rows-[auto_auto_auto]
-            gap-11">
+            gap-11 xl:grid-cols-3 xl:grid-rows-1 ">
         <div className="w-full h-[226px] grid grid-cols-[60%_40%] grid-rows-1
-         items-center border-b-[0.3px] border-gray-300 ">
+         items-center border-b-[0.3px] border-gray-300 xl:border-r-[0.3px] xl:border-b-0  ">
           <div className=" ">
             <div className="mb-5 ">
               <h1 className="text-xl font-normal">
@@ -38,9 +38,9 @@ export const Footer = () => {
             </h1>
           </div>
         </div>
-        
             <div className="h-[249px] *:flex *:flex-row flex flex-col
-                 gap-6 justify-center border-b-[0.3px] border-gray-300 ">
+                 gap-6 justify-center border-b-[0.3px] border-gray-300 
+                 xl:border-r-[0.3px] xl:border-b-0 ">
               <div className="size-auto flex flex-row items-center">
                 <FaLocationDot className="size-6 text-yellow-500  mr-4  "/>
                 <h1 className="text-xl font-normal ">
@@ -60,18 +60,17 @@ export const Footer = () => {
                 </h1>
               </div>
             </div>
-        
-        <div className="size-auto flex flex-row justify-between mb-4">
-          <div className="p-[14px] rounded-[62px] bg-green-600 ">
+        <div className="size-auto flex flex-row justify-between mb-4 items-center ">
+          <div className="size-[60px] rounded-full bg-green-600 flex flex-row justify-center items-center ">
             <FaInstagram className="size-8 text-white " />
           </div>
-          <div className="p-[14px] rounded-[62px] bg-green-600 ">
+          <div className="size-[60px] rounded-full bg-green-600 flex flex-row justify-center items-center">
             <FaFacebookF className="size-8 text-white " />
           </div>
-          <div className="p-[14px] rounded-[62px] bg-green-600 ">
+          <div className="size-[60px] rounded-full bg-green-600 flex flex-row justify-center items-center">
             <FaTwitter className="size-8 text-white " />
           </div>
-          <div className="p-[14px] rounded-[62px] bg-green-600 ">
+          <div className="size-[60px] rounded-full bg-green-600 flex flex-row justify-center items-center">
             <FaWhatsapp className="size-8 text-white " />
           </div>
         </div>
