8b3b408cda50c577d6a0311b6098bc55867b5f34
Rey Messon <rey.messon@holafly.com>
2024-06-06 20:58:19 -0400

    logic

--- src/root/LiveMatch.jsx
+++ src/root/LiveMatch.jsx
@@ -3,6 +3,13 @@ import io from 'socket.io-client';
 
 const socket = io('http://localhost:3000'); // Connect to Socket.io server
 
+var scoreDashData = "0";
+var scoreDashData2 = "0";
+var scoreDataGame = 0;
+var gameRecord = 0;
+var gameRecord2 = 0;
+var changeServiceBall = false
+
 export const LiveMatch = () => {
 
     const [messages, setMessages] = useState([]);
@@ -25,6 +32,91 @@ export const LiveMatch = () => {
         socket.emit('message', input);
         setInput('');
     };
+
+    let products = [{
+        "_id" : "664352312fd3f48a1aec96ff",
+        "images" : [ ],
+        "languages" : [
+                {
+                        "english" : ""
+                },
+                {
+                        "spanish" : ""
+                },
+                {
+                        "german" : ""
+                },
+                {
+                        "french" : ""
+                },
+                {
+                        "italian" : ""
+                },
+                {
+                        "portuguese" : ""
+                }
+        ],
+        "actions" : [
+                {
+                        "id" : "1715702419294",
+                        "date" : "2024-05-20 21:37:59",
+                        "actionId" : "1716255479374",
+                        "action" : "SERVICE_FAULT",
+                        "status" : true,
+                        "player" : "Carlos Florentino",
+                        "game" : 1,
+                        "score" : "0-0",
+                        "gamescore" : "0-0"
+                },
+                {
+                        "id" : "1715702419294",
+                        "date" : "2024-05-20 21:38:00",
+                        "actionId" : "1716255480117",
+                        "action" : "ACE",
+                        "status" : true,
+                        "player" : "Carlos Florentino",
+                        "game" : 2,
+                        "score" : "0-0",
+                        "gamescore" : "0-0"
+                }
+        ],
+        "id" : "1715702419294",
+        "date" : "2024-05-14 12:00:19",
+        "description" : "Prospect_Open24_CarlosFlorentino_VS_AldoLee",
+        "company" : "Carlos Florentino",
+        "company2" : "Aldo Lee",
+        "set" : "",
+        "game" : 2,
+        "visit" : 3,
+        "court" : "",
+        "tiebreak" : "5",
+        "style" : "",
+        "category" : "Prospect_Open24",
+        "favorite" : false,
+        "hidden" : false,
+        "image" : "Prospect_Open24.JPG",
+        "serving" : "Player_1",
+        "__v" : 0
+}]
+
+let filteredData = products.filter(
+
+    (data, index) => data.id == "1715702419294"
+)[0];
+
+
+const result = products.flatMap(data => data.actions).filter( data => data.id == "1715702419294" )
+
+console.log(result)
+
+
+    procesarDatos(result, "Carlos Florentino", "Aldo Lee")
+
+    let scoreNote = ["0","15","30","40","Adv"]
+
+    let scoreField = scoreNote[scoreDashData] + "-" + scoreNote[scoreDashData2]
+
+    console.log(scoreField)
 
     return (
         <section className="w-full h-[100vh] bg-blue-950 " >
@@ -70,4 +162,137 @@ export const LiveMatch = () => {
         </div>
         </section>
     );
+
+
+    
 }
+
+const procesarDatos = (datos, company, company2) => {
+    // Variables para llevar un seguimiento del marcador y los juegos
+    let marcador = { "Eric Daneri": 0, "Martin Genao": 0 };
+    let gameTable = { "Eric Daneri": 0, "Martin Genao": 0 };
+    marcador[company]=0
+    marcador[company2]=0
+    gameTable[company]=0
+    gameTable[company2]=0
+    let gameCompany=0
+    let gameCompany2=0
+    let juegoActual = 0;
+    let deuce = false;
+    const maxIterations = 4;
+    let iterationCount = 0;
+    // Iterar sobre las acciones en el JSON
+    for (const accion of datos) {
+      const jugador = accion.player;
+      const juego = accion.game;
+  
+      // Verificar si el juego ha cambiado
+      if (juego !== juegoActual) {
+        juegoActual = juego;
+        deuce = false;
+      }
+
+    //   console.log(accion.action)
+  
+//     iterationCount++;
+
+//   // Check if the maximum number of iterations has been reached
+//   if (iterationCount >= maxIterations) {
+//     break; // Exit the loop if the limit is reached
+//   }
+
+      // Actualizar el marcador según la acción
+
+        switch (accion.action) {
+            case "WINNER":
+            marcador[jugador]++;
+            break;
+            case "ACE":
+            marcador[jugador]++;
+            break;
+            case "DOUBLE_FAULT":
+                if(accion.player==company2){
+                    marcador[jugador === company ? company2 : company]++;
+                    break;        
+                }else{                    
+                    marcador[jugador === company2 ? company : company2]++;
+                    break;        
+                }
+            case "FORCED_ERROR":
+                if(accion.player==company2){
+                    marcador[jugador === company ? company2 : company]++;
+                    break;        
+                }else{                    
+                    marcador[jugador === company2 ? company : company2]++;
+                    break;        
+                }
+            case "UNFORCED_ERROR":
+                if(accion.player==company2){
+                    marcador[jugador === company ? company2 : company]++;
+                    break;        
+                }else{                    
+                    marcador[jugador === company2 ? company : company2]++;
+                    break;        
+                }
+            case "RETURN_ERROR":
+                if(accion.player==company2){
+                    marcador[jugador === company ? company2 : company]++;
+                    break;        
+                }else{                    
+                    marcador[jugador === company2 ? company : company2]++;
+                    break;        
+                }
+        }
+  
+      // Verificar si estamos en deuce
+      if (marcador[company] >= 3 && marcador[company2] >= 3) {
+        deuce = true;
+      }
+
+      if(marcador[company] > 3 && marcador[company2] > 3){
+        marcador[company] = 3
+        marcador[company2] = 3
+      }
+  
+      // Mostrar el marcador y la acción
+      console.log(`${jugador} - ${marcador[company]}-${marcador[company2]} - ${accion.action} (Game ${juegoActual})`);
+
+      scoreDashData = marcador[company]
+      scoreDashData2 = marcador[company2]
+
+      // Verificar si alguien ganó el juego
+      // console.log( marcador[company]  + " " + marcador[company2] ) 
+      if ((marcador[company] >= 4 || marcador[company2] >= 4) && Math.abs(marcador[company] - marcador[company2]) >= 2) {
+        console.log(`Juego para ${marcador[company] > marcador[company2] ? company : company2}`);
+
+        changeServiceBall=!changeServiceBall
+        if(marcador[company]>marcador[company2]){
+
+            gameTable[company]++
+        }else{
+            
+            gameTable[company2]++
+        }
+        // Reiniciar el marcador
+        marcador[company] = 0;
+        marcador[company2] = 0;
+        scoreDashData=0;
+        scoreDashData2=0;
+        scoreDataGame++;
+
+        // console.log(marcador[company]);
+        // console.log(marcador[company2]);
+        gameRecord = gameTable[company]
+        gameRecord2 = gameTable[company2]
+        console.log( company + " " + gameTable[company])
+        console.log( company2 + " " + gameTable[company2])
+        // console.log( company + " " + gameCompany)
+        // console.log( company2 + " " + gameCompany2)
+      }
+  
+      // Verificar si estamos en deuce y se ha roto el empate
+      if (deuce && Math.abs(marcador[company] - marcador[company2]) === 1) {
+        console.log("Ventaja para " + (marcador[company] > marcador[company2] ? company : company2));
+      }
+    }
+  }
